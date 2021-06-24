2902eee4bbb881c4a4443c9ab63b1650b03deef5
Guntur Sarwohadi <gsarwohadi@gmail.com>
2021-06-24 19:57:46 +0700

    Modifikasi untuk Bagian 3 tutorial YouTube: https://www.youtube.com/watch?v=CfUiK7wRQuY - Tambahkan animasi pada Hero

--- Hero.gd
+++ Hero.gd
@@ -5,6 +5,8 @@ var kecepatan = Vector2.ZERO
 var laju_lompat = -380
 var gravitasi = 12
 
+onready var sprite = $Sprite
+
 func _physics_process(delta):
 	kecepatan.y = kecepatan.y + gravitasi
 	
@@ -16,6 +18,30 @@ func _physics_process(delta):
 	if(Input.is_action_pressed("lompat") and is_on_floor()):
 		kecepatan.y = laju_lompat
 	
-	print(kecepatan.y)
 	kecepatan.x = lerp(kecepatan.x, 0, 0.2)
 	kecepatan = move_and_slide(kecepatan, Vector2.UP)
+	
+	update_animasi()
+
+func update_animasi():
+	if is_on_floor():
+		if kecepatan.x < (laju * 0.5) and kecepatan.x > (-laju * 0.5):
+			sprite.play("Diam")
+		else:
+			sprite.play("Lari")
+	else:
+		if kecepatan.y > 0:
+			# jatuh
+			sprite.play("Jatuh")
+		else:
+			# lompat
+			sprite.play("Lompat")
+	
+	sprite.flip_h = false
+	if kecepatan.x < 0:
+		sprite.flip_h = true
+
+
+
+
+
